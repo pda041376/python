6e303aba278329da504da555c9b937db5dff6854
Patrick Allen <pda041376@yahoo.com>
2015-08-12 09:53:55 -0400

    playing with lists and strings

--- mail.py
+++ mail.py
@@ -12,56 +12,58 @@ import datetime
 
 
 def process_mailbox(mail_item):
-    rv, data = mail_item.search(None, "ALL")
-    if rv != 'OK':
-        print "No messages found!"
-        return
+	rv, data = mail_item.search(None, "ALL")
+	if rv != 'OK':
+		print "No messages found!"
+		return
 
-    for num in data[0].split():
-        rv, data = mail_item.fetch(num, '(RFC822)')
-        if rv != 'OK':
-            print "ERROR getting message", num
-            return
+	for num in data[0].split():
+		rv, data = mail_item.fetch(num, '(RFC822)')
+		if rv != 'OK':
+			print "ERROR getting message", num
+			return
 
-        msg = email.message_from_string(data[0][1])
-        print 'Message %s: %s' % (num, msg['Subject'])
-        print 'Raw Date:', msg['Date']
-        date_tuple = email.utils.parsedate_tz(msg['Date'])
-        if date_tuple:
-            local_date = datetime.datetime.fromtimestamp(email.utils.mktime_tz(date_tuple))
-            print "Local Date:", local_date.strftime("%a, %d %b %Y %H:%M:%S")
+		msg = email.message_from_string(data[0][1])
+		print 'Message %s: %s' % (num, msg['Subject'])
+		print 'Raw Date:', msg['Date']
+		date_tuple = email.utils.parsedate_tz(msg['Date'])
+		if date_tuple:
+			local_date = datetime.datetime.fromtimestamp(email.utils.mktime_tz(date_tuple))
+			print "Local Date:", local_date.strftime("%a, %d %b %Y %H:%M:%S")
 
 
 server_entry = input(("Select the email service\n1: Yahoo Mail\n2: Gmail\n3: Microsoft Email"
                       "(Hotmail, outlook.com, live.com)\n[1, 2, 3, etc.]: "))
 
 if server_entry == 1:
-    mail_item = imaplib.IMAP4_SSL("imap.mail.yahoo.com")
-    account = raw_input("Enter Yahoo email address: ")
-    pwd = getpass.getpass()
+	mail_item = imaplib.IMAP4_SSL("imap.mail.yahoo.com")
+	account = raw_input("Enter Yahoo email address: ")
+	pwd = getpass.getpass()
 elif server_entry == 2:
-    mail_item = imaplib.IMAP4_SSL("imap.gmail.com")
-    account = raw_input("Enter Yahoo email address: ")
-    pwd = getpass.getpass()
+	mail_item = imaplib.IMAP4_SSL("imap.gmail.com")
+	account = raw_input("Enter Gmail address: ")
+	pwd = getpass.getpass()
 elif server_entry == 3:
-    mail_item = imaplib.IMAP4_SSL("imap-mail.outlook.com")
-    account = raw_input("Enter Microsoft email address: ")
-    pwd = getpass.getpass()
+	mail_item = imaplib.IMAP4_SSL("imap-mail.outlook.com")
+	account = raw_input("Enter Microsoft email address: ")
+	pwd = getpass.getpass()
 else:
-    print "Please choose a supported email domain."
-    sys.exit(0)
+	print "Please choose a supported email domain."
+	sys.exit(0)
 
 try:
-    mail_item.login(account, pwd)
+	mail_item.login(account, pwd)
 except imaplib.IMAP4.error:
-    print "LOGIN FAILED!!! "
+	print "LOGIN FAILED!!! "
 
 # next steps: reformat the retrieved mail folder lists
 # and enumerate results
 rv, mailboxes = mail_item.list()
+mailboxes = ''.join(mailboxes).translate(None, ')(/\\"').replace('HasChildren', '') \
+	.replace('HasNoChildren', '')
 if rv == "OK":
-    print "Mailboxes: "
-    print mailboxes
+	print "Mailboxes: "
+	print mailboxes
 
 selection = raw_input("Select a mailbox: ")
 
@@ -71,9 +73,9 @@ selection = raw_input("Select a mailbox: ")
 # implement a way to continue getting messages after the first 20
 rv, data = mail_item.select(selection)
 if rv == "OK":
-    print "Processing mailbox...\n"
-    process_mailbox(mail_item)
-    mail_item.close()
+	print "Processing mailbox...\n"
+	process_mailbox(mail_item)
+	mail_item.close()
 
 # next steps:
 # add mechanism to pick message to view
